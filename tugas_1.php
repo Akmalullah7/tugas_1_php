b4c52582bb8ccaee48e876b0725e474fdba47952
Akmalullah <akmalullah.id@gmail.com>
2023-08-16 15:00:08 +0700

    Tugas 1 PHP
    Dikumpulkan 16 Agustus 2023

---
+++ tugas_1.php
@@ -0,0 +1,25 @@
+<?php
+$goreng = 13000;
+$bakar = 15000;
+$sayur = 13000;
+$tgoreng = 1000;
+$tahgor = 1000;
+$soto = 3000;
+$nasiputih = 5000;
+$teh = 4000;
+$tebu = 5000;
+
+echo ("<h1>Daftar Menu </h1>");
+
+echo ("<ul>
+        <li>Ayam Goreng = Rp $goreng</li> 
+        <li>Ayam Bakar  = Rp $bakar</li> 
+        <li>Ayam Sayur  = Rp $sayur</li> 
+        <li>Tempe Goreng= Rp $tgoreng</li> 
+        <li>Tahu Goreng = Rp $tahgor</li> 
+        <li>Soto        = Rp $soto</li> 
+        <li>Nasi Putih  = Rp $nasiputih</li> 
+        <li>Es Teh      = Rp $teh</li> 
+        <li>Es Tebu     = Rp $tebu</li> 
+        </ul>");
+?>
